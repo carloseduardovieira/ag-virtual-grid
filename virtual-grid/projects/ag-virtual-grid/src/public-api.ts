9e06316a9e2f680bc82acdb071fd0b05c2f64abc
Cvieira <dullvieira@gmail.com>
2021-12-23 12:13:25 +0000

    [fix] table core: importations

--- virtual-grid/projects/ag-virtual-grid/src/public-api.ts
+++ virtual-grid/projects/ag-virtual-grid/src/public-api.ts
@@ -2,6 +2,5 @@
  * Public API Surface of ag-virtual-grid
  */
 
-export * from './lib/ag-virtual-grid.service';
 export * from './lib/ag-virtual-grid.component';
 export * from './lib/ag-virtual-grid.module';
